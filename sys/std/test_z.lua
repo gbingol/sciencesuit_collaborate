d7c47aa7eaac2a88233eb94e8974c044c5e78f51
Gökhan Bingöl <gbingol@hotmail.com>
2021-02-02 12:13:41 +0100

    array support added

--- sys/std/test_z.lua
+++ sys/std/test_z.lua
@@ -6,14 +6,15 @@ local std <const> =std
 local function ztest1(xvec, sd, mu, alternative, conflevel)
 
 	--Default values
-	assert(type(xvec)=="Vector" , "Key x must be Vector.")
+	assert(type(xvec)=="Vector"  or  type(xvec)=="Array" , "Key x must be Vector/Array.")
 	
 	assert(type(sd)=="number" and sd>0,"Key sd must be number >0.")
 	
 	assert(type(mu)=="number","Key mu must be number.")
 	
+	
 	conflevel=conflevel or 0.95
-	assert(type(conflevel)=="number","Key conflevel must be of type number.")
+	assert(type(conflevel)=="number","Key conflevel must be number.")
 	assert(conflevel>=0 and conflevel<=1,"Key conflevel must be in [0,1].")
 
 
@@ -22,8 +23,18 @@ local function ztest1(xvec, sd, mu, alternative, conflevel)
 	alternative=string.lower(alternative)
 	
 	
+	
+	if(type(xvec)=="Array") then
+		xvec=xvec:clone()
+		
+		xvec:keep_numbers()
+	end
+	
+	
+	
 
-	local dim=std.size(xvec) 
+	local dim=#xvec
+	 
 	local xaver=std.mean(xvec)
 	local SE=sd/std.sqrt(dim) ----Standard Error of Mean
 	
@@ -31,27 +42,35 @@ local function ztest1(xvec, sd, mu, alternative, conflevel)
 
 	local alpha=(1-conflevel)
 	
-
 	local zcritical=(xaver-mu)/SE
+	
+	
 	local pvalue=nil
 	local retTable={}
+	
+	
 	
 	if(alternative=="two.sided" or alternative=="notequal") then
 
 		if(zcritical<=0) then
-			pvalue=std.pnorm{q=zcritical}+ (1-std.pnorm{q=math.abs(zcritical)}) --area on the left of zcrit + area on the right of positive
+			--area on the left of zcrit + area on the right of positive
+			pvalue = std.pnorm{q=zcritical} + (1-std.pnorm{q=math.abs(zcritical)}) 
 			
 		elseif(zcritical>0) then
-			pvalue=(1-std.pnorm{q=zcritical}) +std.pnorm{q=-zcritical} --area on the right of positive zcriticial + area on the left of negative zcriticial
+			--area on the right of positive zcriticial + area on the left of negative zcriticial
+			pvalue = (1-std.pnorm{q=zcritical}) +std.pnorm{q=-zcritical} 
 		end
+		
 		
 		retTable.CI_upper=xaver-std.qnorm{p=alpha/2.0}*SE
 		retTable.CI_lower=xaver+std.qnorm{p=alpha/2.0}*SE
+
 
 	elseif(alternative=="greater") then
 		pvalue=(1-std.pnorm{q=zcritical}) -- area on the right
 		
 		retTable.CI_lower=xaver+std.qnorm{p=alpha}*SE
+
 
 	elseif(alternative=="less") then
 		pvalue=std.pnorm{q=zcritical} --area on the left
@@ -59,8 +78,10 @@ local function ztest1(xvec, sd, mu, alternative, conflevel)
 		retTable.CI_upper=xaver-std.qnorm{p=alpha}*SE
 		
 	else
-		error("ERROR: The values for the argument 'alternative': \"two.sided\" or \"notequal\", \"greater\", \"less\"", std.const.ERRORLEVEL)
+		error("Values for arg 'alternative': \"two.sided\" or \"notequal\", \"greater\", \"less\"", std.const.ERRORLEVEL)
 	end
+
+
 
 
 	retTable.SE=SE
@@ -85,7 +106,7 @@ local function ztest(...)
 	
 	if(#args==1 and type(args[1])=="table") then
 		
-		local xvec, alternative, mu, sd, conflevel=nil, nil, nil, nil,nil
+		local xvec, alternative, mu, sd, conflevel = nil, nil, nil, nil,nil
 		local NArgsTbl=0
 		
 		for k,v in pairs(args[1]) do
@@ -104,13 +125,15 @@ local function ztest(...)
 		
 		end
 
-		assert(NArgsTbl>0,"ERROR: Keys: x, alternative, mu, sd, conflevel.")
+
+		assert(NArgsTbl>0,"Keys: x, alternative, mu, sd, conflevel.")
+		
 		
 		return ztest1(xvec, sd, mu, alternative, conflevel)
 		
 	else
 	
-		error("Arg must be a Lua table with keys: x, alternative, mu, sd, conflevel.")
+		error("Arg must be a Lua table with keys: x, alternative, mu, sd, conflevel.", std.const.ERRORLEVEL)
 
 	end
 			
