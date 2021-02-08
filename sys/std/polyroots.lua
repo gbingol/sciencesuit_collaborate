871b968e01bea7ee31d5e3d6acab5bf7fefa6e5c
Gökhan Bingöl <gbingol@hotmail.com>
2021-02-08 19:42:44 +0100

    polyroot limitation on monic poly lifted

--- sys/std/polyroots.lua
+++ sys/std/polyroots.lua
@@ -2,19 +2,32 @@ local std <const> =std
 
 
 
---currently just monic polynom algorithm implemented
+--currently only monic polynom algorithm implemented
 
 
-local function POLYROOTS_MONICPOLY(vec)
+local function IsMonicPoly(vec)
+	return math.abs(vec(1) - 1 ) < std.const.tolerance
+end
+
+
+
+local function MONICPOLY(vec)
 	
-	--vec is in the form of monic polynomial
+	--vec is in the form of anxn+...+a0 polynomial
 	
 	--uses eigen value approach
 	--Companion is the companion matrix for a polynomial
 	
 	
 	assert(type(vec) == "Vector", "Argument must be Vector")
-	assert(std.abs(vec(1) -1 ) <std.const.tolerance, "A monic polynomial is expected.") 
+	
+	
+	--If not monic polynomial, make it monic polynomial
+	if(IsMonicPoly(vec) == false) then
+		vec = vec/vec(1)
+	end
+		
+		
 	
 	local dim = #vec
 	
@@ -25,8 +38,9 @@ local function POLYROOTS_MONICPOLY(vec)
 	
 	Companion:insert(Zeros, 1, "col")  -- (dim-2) by (dim-1) 
 	
-	vec = -vec	
+	
 	vec[1] = nil
+	vec = -vec	
 	vec:reverse()
 	
 	Companion:append(vec, "row") -- (dim -1) * (dim-1)
@@ -38,7 +52,70 @@ end
 
 
 
-std.polyroots=POLYROOTS_MONICPOLY
+
+
+
+local function POLYROOTS(...)
+	
+	local arg=table.pack(...)
+	
+	if(#arg==1  and type(arg[1])=="table") then
+		
+		local Polynom, method = nil, "eigen"
+		local NTblArgs=0
+
+		for k,v in pairs(arg[1]) do
+			
+			local key=string.lower(k)
+			
+			if(key=="poly") then Polynom = v
+			elseif(key=="method") then method =string.lower(v)
+			
+			else 
+				error("Usage: {poly=, [method=\"eigen\"]}", std.const.ERRORLEVEL)
+			end
+
+			NTblArgs=NTblArgs+1
+
+		end
+		
+		assert(NTblArgs >= 1, "Usage: {poly=, [method=\"eigen\"]}")
+		assert(Polynom ~= nil, "polynomial coefficents undefined")
+		
+		assert(type(method) == "string", "method must be string")
+
+		if(method =="eigen") then
+			return MONICPOLY(Polynom)
+		end
+
+	
+	elseif (#arg==1) then
+		return MONICPOLY(arg[1])
+
+	elseif (#arg==2) then
+		local Polynom = arg[1]
+		local method = arg[2]
+		
+		assert(type(method) == "string", "2nd arg must be string")
+		
+		if(method =="eigen") then
+			return MONICPOLY(Polynom)
+		else
+			error("Undefined method", std.const.ERRORLEVEL)
+		end
+	end
+
+
+	error("Usage: {poly=, [method=\"eigen\"]} or (poly=, method=\"eigen\")", std.const.ERRORLEVEL)
+
+end
+
+
+
+
+
+
+std.polyroots = POLYROOTS
 	
 	
 
