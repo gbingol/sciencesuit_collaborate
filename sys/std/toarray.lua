3b0cde4367993cc56e887e2193164f74d2574506
Gökhan Bingöl <gbingol@hotmail.com>
2021-02-26 13:46:44 +0100

    toarray checks if container is iteratable and 2nd param controls whether to attemp to convert string to variant

--- sys/std/toarray.lua
+++ sys/std/toarray.lua
@@ -1,30 +1,68 @@
 local std <const> =std
 
 
+local function stringtovariant(str)
+	local realNum=tonumber(str)
+	
+	if(realNum ~=nil) then
+		return realNum
+	end
 
-local function toarray(Container, Arr)
+	local success, complexNum=pcall(std.Complex.new, str)
+	
+	if(success) then
+		return complexNum
+	end
+
+	return str
+end
+
+
+
+local function toarray(Container, StrToType, Arr)
 
 	--If the entry is a matrix then it returns the elements in a row-order sequence
 	--If the entry is a 1D table then it is converted to vector
+	
+	--StrToType: By default it is true and if true, then string entries will be attempted to convert to real or complex number type
+	
+	assert(std.util.isiteratable(Container) == true, "Iteratable container expected")
 	
 	if(Arr==nil) then
 		Arr=std.Array.new(0)
 	end
 
+	if(StrToType == nil) then
+		StrToType=true
+	end
+
+	assert(type(StrToType)=="boolean", "Second arg must be boolean")
+
 	for k, v in pairs(Container) do
 		
 		local typ=type(v)
 		
+		--basic types
 		if(typ=="number" or typ=="string" or typ=="Complex") then
-			Arr:push_back(v)
+			
+			--can we convert the string to number of complex number
+			if(typ == "string" and StrToType == true) then
+				Arr:push_back(stringtovariant(v))
+			
+			--push as is
+			else
+				Arr:push_back(v)
+			end
+
+		--an iteratable container, i.e. matrix, vector, table...
 		else
 			toarray(v, Arr)
 		end
 		
-	end
+	end --for k, v
+
 
 	return Arr
-
 end
 
 
